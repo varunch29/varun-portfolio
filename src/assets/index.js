ac8ca8a61c958a2d8aadf5733eea90cd06ada7f7
varunch29 <varunreddynani29@gmail.com>
2025-02-08 00:24:18 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import parthmittal from "./parth.svg";
+import profilePic from "./profile-pic.jpg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import arrowUp from "./arrow-up.svg";
+import nitk from "./nitk.png";
+import cluboard from "./cluboard.png";
+import cash_flow from "./cash_flow.jpg";
+import ecell from "./ecell.png";
+import gdsc from "./gdsc.png";
+import iris from "./iris.png";
+import genesis from "./genesis.jpg";
+import portfolio from "./portfolio.jpg";
+import cdc from "./cdc.png";
+import chargeswap from "./chargeswap.png";
+import placeicon from "./place-icon.svg";
+import recruitment from "./recruitment.png";
+import huntly from "./huntly.png";
+import oracle from "./oracle.jpg";
+import comicify_ai from "./comicify-ai.png";
+import greentrust from "./green_trust.png";
+import averlon from "./averlon.jpg";
+import devfolio from "./devfolio.png";
+import pba from "./pba_logo.jpeg";
+import ethglobal from "./ethglobal.png";
+import polkadot from "./polkadot.jpeg";
+import lightspeed from "./lightspeed.png";
+import dennisivy from "./dennisivy.png";
+import manipal from "./manipal.png";
+import icon from "./icon.jpg";
+import ethforall from "./ethforall.png";
+import ethindia from "./ethindia.png";
+
+export {
+  menu,
+  close,
+  arrowUp,
+  nitk,
+  cluboard,
+  cash_flow,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  portfolio,
+  cdc,
+  parthmittal,
+  profilePic,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  ethindia
+};
